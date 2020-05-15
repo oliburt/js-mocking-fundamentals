f016fc3ff22079a0a9fd6f0ab575add1a2e78110
oliburt <oliburt@gmail.com>
2020-05-15 16:50:48 +0100

    monkey patch

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,12 +1,19 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
+const assert = require("assert")
+const thumbWar = require("../thumb-war")
+const utils = require("../utils")
 
+
+// Here we monkey patch the supposed random/expenisve 
+// function (which could be third party) and we are 
+// trying to test the functionality of our own function 
+// so we replace this function with something 
+// predictable
 const originalGetWinner = utils.getWinner
-utils.getWinner = (p1, p2) => p1
+utils.getWinner = (a, b) => b
 
-const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-assert.strictEqual(winner, 'Kent C. Dodds')
+const winner = thumbWar("Olib", "Olia")
 
-// cleanup
+assert.strictEqual(winner, "Olia")
+
+// restore/cleanup the module to original state
 utils.getWinner = originalGetWinner
